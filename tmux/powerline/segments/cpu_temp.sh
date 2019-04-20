c7c843d4be52b9c52008c912521a768e6af60acc
Carlos Hernandez <carlos@techbyte.ca>
2019-04-20 17:12:11 -0600

    Update ryzen cpu temp

--- tmux/powerline/segments/cpu_temp.sh
+++ tmux/powerline/segments/cpu_temp.sh
@@ -6,7 +6,7 @@ run_segment() {
 		return
 	fi
 
-	temp=$(sensors | awk '{ if($0 ~ "k10temp"){ amd=1 } if(amd){ if($1 ~ "^temp1*"){print $2; exit;}} if($0 ~ "^coretemp"){ intel=1 } if(intel){ if($1 ~ "^Package"){print $4; exit} } }')
+	temp=$(sensors | awk '{ if($0 ~ "k10temp"){ amd=1 } if(amd){ if($1 ~ "^Tdie*"){print $2; exit;}} if($0 ~ "^coretemp"){ intel=1 } if(intel){ if($1 ~ "^Package"){print $4; exit} } }')
 	if [ -n "$temp" ]; then
 		echo "$temp";
 	fi
