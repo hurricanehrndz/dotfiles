f9c2d60aa96176266b39f6fd4e4b059e30d76cfe
Carlos Hernandez <carlos@techbyte.ca>
2019-06-28 12:51:13 -0600

    Remove tmux config, personal zfunc

--- tmux/powerline/segments/cpu_usage.sh
+++
@@ -1,8 +0,0 @@
-# Print the average load.
-
-run_segment() {
-  usage=$(mpstat | awk '{ if ($3 == "all") { idle=$13; print (100 - idle)"%"; } }')
-  load=$(uptime | cut -d "," -f 3- | cut -d ":" -f2 | sed -e "s/^[ \t]*//")
-  echo "$usage $load";
-  return 0
-}
