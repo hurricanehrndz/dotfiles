8f5c4ab348a9da0b489d96f2bbce04f63e869f9c
Carlos Hernandez <carlos@techbyte.ca>
2019-07-15 09:43:50 -0600

    Add git aliases update zsh antibody plugins

---
+++ config/zsh/git/aliases.zsh
@@ -0,0 +1,24 @@
+#!/usr/bin/env zsh
+# vim:set sw=2 sts=2 ts=2 et:
+#
+if (( ! $+commands[git] )); then
+  return
+fi
+
+_git_log_oneline_format='%C(green)%h%C(reset) %s%C(red)%d%C(reset)%n'
+
+alias gcl='git clone --recursive'
+alias gf='git fetch'
+alias gco='git checkout'
+alias gcm='git checkout master'
+alias gst='git status'
+alias gss='git status --short'
+alias glg='git log --topo-order --all --graph --pretty=format:"${_git_log_oneline_format}"'
+alias ggrh='git reset --hard origin/$(current_branch)'
+alias gd='git diff'
+alias gdc='git diff --cached'
+alias gc='git commit'
+alias gca='git commit -a'
+alias ga='git add'
+alias gca!='git commit -a --amend'
+alias ggpush='git push origin $(current_branch)'
--- config/zsh/zsh_plugins.sh
+++ config/zsh/zsh_plugins.sh
@@ -4,12 +4,15 @@ fpath+=( /home/hurricanehrndz/.local/share/antibody/https-COLON--SLASH--SLASH-gi
 
 source /home/hurricanehrndz/.local/share/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-belak-SLASH-zsh-utils/history/history.plugin.zsh
 fpath+=( /home/hurricanehrndz/.local/share/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-belak-SLASH-zsh-utils/history )
-source /home/hurricanehrndz/.local/share/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-sorin-ionescu-SLASH-prezto/modules/git/alias.zsh
+source /home/hurricanehrndz/.local/share/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-lukechilds-SLASH-zsh-nvm/zsh-nvm.plugin.zsh
+fpath+=( /home/hurricanehrndz/.local/share/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-lukechilds-SLASH-zsh-nvm )
 source /home/hurricanehrndz/.local/share/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-belak-SLASH-zsh-utils/completion/completion.plugin.zsh
 fpath+=( /home/hurricanehrndz/.local/share/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-belak-SLASH-zsh-utils/completion )
 source /home/hurricanehrndz/.local/share/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-woefe-SLASH-git-prompt.zsh/git-prompt.plugin.zsh
 fpath+=( /home/hurricanehrndz/.local/share/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-woefe-SLASH-git-prompt.zsh )
-source /home/hurricanehrndz/.local/share/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zdharma-SLASH-fast-syntax-highlighting/fast-syntax-highlighting.plugin.zsh
-fpath+=( /home/hurricanehrndz/.local/share/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zdharma-SLASH-fast-syntax-highlighting )
+source /home/hurricanehrndz/.local/share/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-syntax-highlighting/zsh-syntax-highlighting.plugin.zsh
+fpath+=( /home/hurricanehrndz/.local/share/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-syntax-highlighting )
 source /home/hurricanehrndz/.local/share/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-history-substring-search/zsh-history-substring-search.plugin.zsh
 fpath+=( /home/hurricanehrndz/.local/share/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-history-substring-search )
+source /home/hurricanehrndz/.local/share/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-autosuggestions/zsh-autosuggestions.plugin.zsh
+fpath+=( /home/hurricanehrndz/.local/share/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-autosuggestions )
