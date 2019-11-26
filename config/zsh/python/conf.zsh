f9037c6ebb073e876667dbafbcb7f47fc4889145
Carlos Hernandez <carlos@techbyte.ca>
2019-11-26 15:33:22 -0700

    Disable pyenv lazy loading

--- config/zsh/python/conf.zsh
+++ config/zsh/python/conf.zsh
@@ -2,30 +2,12 @@
 # vim:set sw=2 sts=2 ts=2 et:
 
 
-# virtualenvwrapper
-if [[ -s "/usr/share/virtualenvwrapper/virtualenvwrapper_lazy.sh" ]]; then
-  export WORKON_HOME="$HOME/.virtualenvs"
-  # pure magic number
-  export VIRTUAL_ENV_DISABLE_PROMPT=12
-  source "/usr/share/virtualenvwrapper/virtualenvwrapper_lazy.sh"
-fi
+export VIRTUAL_ENV_DISABLE_PROMPT=12
 
 [[ -s "${HOME}/.local/share/pyenv/bin/pyenv" ]] || return
 
 export PYENV_ROOT="$HOME/.local/share/pyenv"
 path=("$PYENV_ROOT/bin" $path)
 
-# lazy load pyenv
-__pyenv_started=0
+eval "$(command pyenv init - --no-rehash zsh)"
 
-__pyenv_init() {
-  test $__pyenv_started = 0 && {
-    eval "$(command pyenv init - --no-rehash zsh)"
-    __pyenv_started=1
-  }
-}
-
-pyenv() {
-  __pyenv_init
-  command pyenv "$@"
-}
