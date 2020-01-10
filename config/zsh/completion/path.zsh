e0fd995f3c222506db21e6b43e71671140cf99a7
Carlos Hernandez <carlos@techbyte.ca>
2020-01-09 20:57:00 -0700

    add vendor-completions to fpath

--- config/zsh/completion/path.zsh
+++ config/zsh/completion/path.zsh
@@ -1,3 +1,7 @@
 # vim:set ft=zsh sw=2 sts=2 ts=2 et:
 
 fpath=($HOME/.local/share/zsh/completions $fpath)
+
+if [[ -e "/usr/share/zsh/vendor-completions" ]]; then
+  fpath=(/usr/share/zsh/vendor-completions $fpath)
+fi
