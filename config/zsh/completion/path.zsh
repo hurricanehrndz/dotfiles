9c97e155cdcbc4f6e2efa9e053255492dc42d9db
Carlos Hernandez <carlos@techbyte.ca>
2020-04-12 10:32:26 -0600

    Add site-function dir to completion

--- config/zsh/completion/path.zsh
+++ config/zsh/completion/path.zsh
@@ -5,3 +5,7 @@ fpath=($HOME/.local/share/zsh/completions $fpath)
 if [[ -e "/usr/share/zsh/vendor-completions" ]]; then
   fpath=(/usr/share/zsh/vendor-completions $fpath)
 fi
+
+if [[ -e "/usr/share/zsh/site-functions" ]]; then
+  fpath=(/usr/share/zsh/site-functions $fpath)
+fi
