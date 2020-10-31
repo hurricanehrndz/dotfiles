6c05ba2c06f6ea0629ceab2922648a3198c1b264
Carlos Hernandez <carlos@techbyte.ca>
2020-10-31 15:52:34 -0600

    Add ~/.yarn/bin to path when applicable.

--- config/zsh/node/path.zsh
+++ config/zsh/node/path.zsh
@@ -6,3 +6,9 @@ FNM_DIR="${HOME}/.local/share/fnm"
 test -d "$FNM_DIR" || return 0
 
 path=("$FNM_DIR" $path)
+
+YARN_BIN_PATH="${HOME}/.yarn/bin"
+
+test -d "$YARN_BIN_PATH" || return 0
+
+path=("$YARN_BIN_PATH" $path)
