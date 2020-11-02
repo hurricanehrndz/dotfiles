90fe3f786e9e9be0abd15675505e5177a14c2527
Carlos Hernandez <carlos@techbyte.ca>
2020-11-02 14:27:04 -0700

    Init fnm quietly

--- config/zsh/node/init.zsh
+++ config/zsh/node/init.zsh
@@ -2,6 +2,6 @@
 # vim:set ft=zsh sw=2 sts=2 ts=2 et:
 
 if (( $+commands[fnm] )); then
-  eval $(fnm env --shell=zsh)
+  eval $(fnm --fnm-dir=$FNM_DIR --log-level=quiet env --shell=zsh)
   [[ -e "$FNM_DIR/aliases/default" ]] && fnm use default
 fi
