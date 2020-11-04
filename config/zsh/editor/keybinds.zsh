0a53b741d6b9a579fc03bb5d779450866b592192
Carlos Hernandez <carlos@techbyte.ca>
2020-11-04 11:05:57 -0700

    Add more vi-mode keybinds
    Navigate history with <c-j> and <c-k>. Jump to beginning of line and end
    of line with Ctrl+6 and Ctrl+4.

---
+++ config/zsh/editor/keybinds.zsh
@@ -0,0 +1,7 @@
+#!/usr/bin/env zsh
+# vim:set ft=zsh sw=2 sts=2 ts=2 et:
+
+bindkey -M viins  '^K'    history-substring-search-up
+bindkey -M viins  '^J'    history-substring-search-down
+bindkey -M viins  '^\'    end-of-line
+bindkey -M viins  '^^'    beginning-of-line
