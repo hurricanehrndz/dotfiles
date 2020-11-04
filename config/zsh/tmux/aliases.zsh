977c5031f75effda1a88e8485d6383c5002b283f
Carlos Hernandez <carlos@techbyte.ca>
2020-11-04 13:06:23 -0700

    Update tmux aliases and config

--- config/zsh/tmux/aliases.zsh
+++ config/zsh/tmux/aliases.zsh
@@ -4,9 +4,9 @@
 TMUX_CONF="~/.config/tmux/tmux.conf"
 
 alias                               \
-  tn="tmux new"    \
-  ta="tmux attach" \
-  tm="tmux"        \
+  tn="tmux -u -f $TMUX_CONF new"    \
+  ta="tmux -u -f $TMUX_CONF attach" \
+  tm="tmux -u -f $TMUX_CONF"        \
   tt="nvim $TMUX_CONF"
 
 alias tsheldon='sheldon --data-dir "$HOME/.config/tmux/plugins" --config-dir "$HOME/.config/tmux"'
