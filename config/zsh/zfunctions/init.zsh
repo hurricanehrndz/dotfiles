d63f196bd74500ee448788608bb15a4eefc65cf6
Carlos Hernandez <carlos@techbyte.ca>
2020-11-04 05:45:39 -0700

    Move autoloading of functions to init

---
+++ config/zsh/zfunctions/init.zsh
@@ -0,0 +1,10 @@
+#!/usr/bin/env zsh
+# vim:set ft=zsh sw=2 sts=2 ts=2 et:
+
+# load personal functions
+autoload randpw
+autoload gi
+autoload timezsh
+autoload update-fnm
+autoload cargo-reinstall-all
+autoload install-fav-crates
--- config/zsh/zfunctions/path.zsh
+++ config/zsh/zfunctions/path.zsh
@@ -1,9 +1,4 @@
+#!/usr/bin/env zsh
 # vim:set ft=zsh sw=2 sts=2 ts=2 et:
 
 fpath=($ZDOTDIR/zfunctions $fpath)
-autoload randpw
-autoload gi
-autoload timezsh
-autoload update-fnm
-autoload cargo-reinstall-all
-autoload install-fav-crates
