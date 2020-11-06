789d02dddea4b416ff4d4042388e7224d9f6a3fa
Carlos Hernandez <carlos@techbyte.ca>
2020-11-06 07:19:53 -0700

    Set FZF_DEFAULT_OPTS

--- config/zsh/fzf/init.zsh
+++ config/zsh/fzf/init.zsh
@@ -7,6 +7,8 @@ if [[ -s "$HOME/.local/share/fzf/shell/key-bindings.zsh" ]]; then
   source "$HOME/.local/share/fzf/shell/key-bindings.zsh"
 fi
 
+export FZF_DEFAULT_OPTS='--height 40% --layout=reverse --border'
+
 fman() {
   man -k . | fzf --prompt='Man> ' | awk '{print $1}' | xargs -r man
 }
