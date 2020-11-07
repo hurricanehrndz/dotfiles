7a223bdf28997542c53a08c8d28a829d37b18979
Carlos Hernandez <carlos@techbyte.ca>
2020-11-07 13:04:23 -0700

    Simplifying path

--- config/zsh/profile/path.zsh
+++ config/zsh/profile/path.zsh
@@ -1,15 +1,12 @@
 #!/usr/bin/env zsh
 # vim:set ft=zsh sw=2 sts=2 ts=2 et:
 
-if [[ -d "$HOME/go" ]]; then
-  export GOPATH=$HOME/go
-  path=("$GOPATH/bin" $path)
-fi
+FNM_DIR="${HOME}/.local/share/fnm"
 
-if [[ -d "$HOME/.cargo/bin"  ]]; then
-  path=("$HOME/.cargo/bin" $path)
-fi
-
-if [[ -d "$HOME/.local/bin" ]]; then
-  path=("$HOME/.local/bin" $path)
-fi
+path=(               \
+  "$GOPATH/bin"      \
+  "$HOME/.cargo/bin" \
+  "$HOME/.local/bin" \
+  "$HOME/.yarn/bin"  \
+  $path              \
+)
