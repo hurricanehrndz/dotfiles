806d10359701407d99495c56d53e8c94aed8d29e
Carlos Hernandez <carlos@techbyte.ca>
2020-12-03 11:22:54 -0700

    Fix zsh key-bindings and set mode to vi

--- config/zsh/editor/keybinds.zsh
+++ config/zsh/editor/keybinds.zsh
@@ -1,10 +1,14 @@
 #!/usr/bin/env zsh
 # vim:set ft=zsh sw=2 sts=2 ts=2 et:
 
-bindkey   -M   viins   '^K'   history-substring-search-up
-bindkey   -M   viins   '^J'   history-substring-search-down
-bindkey   -M   viins   '^\'   end-of-line
-bindkey   -M   viins   '^^'   beginning-of-line
-bindkey   -M   viins   '^Y'   autosuggest-accept
-bindkey   -M   vicmd   'k'    history-substring-search-up
-bindkey   -M   vicmd   'j'    history-substring-search-down
+# my vim like keybinds
+bindkey   -M   viins   "$key_info[Control]K"   history-substring-search-up
+bindkey   -M   viins   "$key_info[Control]J"   history-substring-search-down
+bindkey   -M   viins   "$key_info[Control]\\"   end-of-line
+bindkey   -M   viins   "$key_info[Control]^"   beginning-of-line
+bindkey   -M   viins   "$key_info[Control]Y"   autosuggest-accept
+bindkey   -M   vicmd   "k"                     history-substring-search-up
+bindkey   -M   vicmd   "j"                     history-substring-search-down
+
+# set vi mode
+bindkey -v
