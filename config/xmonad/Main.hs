89357d4c3f5021e2768eaddbf55b41aedfc2ed37
Carlos Hernandez <carlos@techbyte.ca>
2020-12-08 11:54:05 -0700

    Add personalize xmobar config
    Add xmobar config that supports output from xmonad. Updating xmonad
    config/build to support dynamicLog hook required to for xmonad info in
    xmobar.

--- config/xmonad/Main.hs
+++ config/xmonad/Main.hs
@@ -1,24 +1,29 @@
--- vim: set sw=4 et:
-  -- Base
-import XMonad
+  -- darcs starter config
 import System.IO
+import XMonad
+  -- Support modern browsers, panels and trays
+  -- Toggle panel visibility (mod+b)
+import XMonad.Config.Desktop
 
   -- Hooks
 import XMonad.Hooks.DynamicLog
-import XMonad.Hooks.ManageDocks
 
   -- Utilities
 import XMonad.Util.Run(spawnPipe)
-import XMonad.Util.EZConfig(additionalKeys)
+--import XMonad.Util.EZConfig(additionalKeys)
+
+-- my config
+baseConfig = desktopConfig {
+  modMask = mod4Mask,         -- Use Super instead of Alt
+  terminal = "alacritty"
+  }
 
 main :: IO ()
 main = do
-    xmproc <- spawnPipe "xmobar"
-    xmonad $ docks defaults
-
--- my config
-defaults = def {
-        modMask = mod4Mask,         -- Use Super instead of Alt
-        terminal = "alacritty",
-        layoutHook = avoidStruts $ layoutHook def
+  xmproc <- spawnPipe "xmobar $HOME/.config/xmobar/xmobarrc"
+  xmonad $ baseConfig {
+    logHook = logHook baseConfig <+> dynamicLogWithPP xmobarPP {
+      ppOutput = hPutStrLn xmproc,
+      ppTitle = xmobarColor "green" "" . shorten 40 . xmobarStrip
+      }
     }
