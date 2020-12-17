b1ec8c4147e3cd59276a79a34d072be30fc0977a
Carlos Hernandez <carlos@techbyte.ca>
2020-12-17 13:57:45 +0000

    Fix install of nixos fonts

--- config/nixos/common/fonts.nix
+++ config/nixos/common/fonts.nix
@@ -4,14 +4,11 @@
   # Install some fonts system-wide, especially "Source Code Pro" in the
   # Nerd-Fonts pached version with extra glyphs.
   fonts = {
-    fontDir.enable = true;
+    enableDefaultFonts = true;
     fonts = with pkgs; [
       (nerdfonts.override { fonts = [ "FiraCode" "Hack" "SourceCodePro" ]; })
       joypixels
       noto-fonts
-      noto-fonts-cjk
-      liberation_ttf
-      noto-fonts-emoji
       corefonts
     ];
   };
