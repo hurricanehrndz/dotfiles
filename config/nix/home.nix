9174970ee79c0b5831efd3aa454317a63ac65a5c
Carlos Hernandez <carlos@techbyte.ca>
2020-12-18 15:23:50 -0700

    Start setting up zsh via home-manager

--- config/nix/home.nix
+++ config/nix/home.nix
@@ -3,17 +3,54 @@
 {
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
-  # Enable XDG User Directories
-  xdg.enable = true;
+
   # Configure nix
   nixpkgs.config.allowUnfree = true;
   xdg.configFile."nixpkgs/config.nix".source = ./nixpkgs.nix;
 
   home.packages = with pkgs; [
-    # Basic tools
+    # My cmdline utils
+    bottom
+    exa
     htop
     ripgrep
+
+    # nix stuff
+    nix-zsh-completions
   ];
 
+  # Enable shell management
+  programs.zsh = {
+    enable = true;
+    enableAutosuggestions = true;
+    enableCompletion = true;
+    defaultKeymap = "viins";
+    dotDir = ".config/zsh";
+    history = {
+      expireDuplicatesFirst = true;
+      path = ".config/zsh/.zsh_history";
+    };
+  };
+  programs.bat.enable = true;
+  programs.skim = {
+    enable = true;
+    enableZshIntegration = true;
+  };
+  programs.starship = {
+    enable = true;
+    enableZshIntegration = true;
+    settings = {
+      directory = {
+        truncate_to_repo = false;
+        fish_style_pwd_dir_length = 1;
+      };
+      cmd_duration.disabled = true;
+    };
+  };
+
+  # Enable XDG User Directories
+  xdg.enable = true;
+
+  # Session variables
   home.sessionVariables = { EDITOR = "nvim"; };
 }
